ea1e8640a3cc45ce795e2d29fe87ded6c0b6023a
Dinis Lage <dinislage@senolage.com>
2015-04-01 13:44:16 +0100

    fix use base to SOAP::WSDL::SOAP::Header

--- lib/SOAP/WSDL/SOAP/HeaderFault.pm
+++ lib/SOAP/WSDL/SOAP/HeaderFault.pm
@@ -1,7 +1,7 @@
 package SOAP::WSDL::SOAP::HeaderFault;
 use strict;
 use warnings;
-use base qw(SOAP::WSDL::Header);
+use base qw(SOAP::WSDL::SOAP::Header);
 
 our $VERSION = $SOAP::WSDL::VERSION;
 
